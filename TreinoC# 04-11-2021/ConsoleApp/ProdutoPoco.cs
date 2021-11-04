8206025c2b345c5323470292e9c83dd5fa85d6fe
HELIO AKIRA DE QUEIROZ <84886134+HellAkira@users.noreply.github.com>
2021-11-04 15:30:21 -0400

    Add files via upload

---
+++ TreinoC# 04-11-2021/ConsoleApp/ProdutoPoco.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace ConsoleApp
+{
+    public class ProdutoPoco : CamposComuns
+    {
+    private int produtoID;
+    
+    private int subCategoriaID;
+
+    private int categoriaID;
+
+        public int ProdutoID 
+        { 
+            get => produtoID; 
+            set => produtoID = value; 
+            }
+        public int SubCategoriaID 
+        { 
+            get => subCategoriaID; 
+            set => subCategoriaID = value; 
+        }
+        public int CategoriaID 
+        { 
+            get => categoriaID; 
+            set => categoriaID = value; 
+        }
+    }
+}
---
+++ TreinoC# 04-11-2021/ConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EstoqueLambda estoque = new EstoqueLambda();
+            estoque.TestarExibicao();
+            Console.ReadLine();
+        }
+    }
+}
